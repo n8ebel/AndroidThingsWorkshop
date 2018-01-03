186ac17cc946f8ff1ab8a443f3e437897d793359
n8ebel <nate@udacity.com>
2018-01-02 17:00:48 -0800

    Hello World
    Includes a button that, when pressed, shows a toast

---
+++ app/src/main/java/com/n8/helloandroidthings/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.n8.helloandroidthings
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+/**
+ * Skeleton of an Android Things activity.
+ *
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ *
+ * <pre>{@code
+ * val service = PeripheralManagerService()
+ * val mLedGpio = service.openGpio("BCM6")
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+ * mLedGpio.value = true
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ *
+ */
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button:Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Hello World", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
