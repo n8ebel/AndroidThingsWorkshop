572293add54887af41be165cd4621f48e774e211
n8ebel <nate@udacity.com>
2018-02-02 20:43:52 -0800

    Connect Red, Green, Blue LEDs to Buttons A,B, and C

--- HelloRainbowHat/app/src/main/java/com/n8/hellorainbowhat/MainActivity.kt
+++ HelloRainbowHat/app/src/main/java/com/n8/hellorainbowhat/MainActivity.kt
@@ -2,17 +2,43 @@ package com.n8.hellorainbowhat
 
 import android.app.Activity
 import android.os.Bundle
-
-private val TAG = MainActivity::class.java.simpleName
+import com.google.android.things.contrib.driver.button.Button
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
+import com.google.android.things.pio.Gpio
 
 class MainActivity : Activity() {
 
+    val buttonA:Button by lazy { RainbowHat.openButtonA() }
+    val buttonB:Button by lazy { RainbowHat.openButtonB() }
+    val buttonC:Button by lazy { RainbowHat.openButtonC() }
+
+    val ledRed:Gpio by lazy { RainbowHat.openLedRed() }
+    val ledGreen:Gpio by lazy { RainbowHat.openLedGreen() }
+    val ledBlue:Gpio by lazy { RainbowHat.openLedBlue() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // turn off all leds
+        ledRed.value = false
+        ledGreen.value = false
+        ledBlue.value = false
+
+        // turn on an led when each button is pressed
+        buttonA.setOnButtonEventListener { button, pressed -> ledRed.value = pressed }
+        buttonB.setOnButtonEventListener { button, pressed -> ledGreen.value = pressed }
+        buttonC.setOnButtonEventListener { button, pressed -> ledBlue.value = pressed }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        buttonA.close()
+        buttonB.close()
+        buttonC.close()
+
+        ledRed.close()
+        ledGreen.close()
+        ledBlue.close()
     }
 
 }
